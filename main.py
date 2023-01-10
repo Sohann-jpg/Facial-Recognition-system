555cbe16bd5bb8fd57f0433846d1d31a33e70519
Suhan Shrestha <83650068+Sohann-jpg@users.noreply.github.com>
2023-01-10 16:24:48 +0545

    Add files via upload
    Changes made on Jan 10

---
+++ main.py
@@ -0,0 +1,113 @@
+from tkinter import * 
+from tkinter import ttk
+from PIL import Image,ImageTk
+
+# Making a class that initiates the backend application of the project
+class Face_Recognition_System:
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry("1530x790+0+0")
+        self.root.title("Face Recognition System")
+        # Adding a header image to the application software
+        img = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Header.jpg")
+        img = img.resize((1300, 130), Image.ANTIALIAS)
+        self.photoimg = ImageTk.PhotoImage(img)
+        f_lbl = Label(self.root, image = self.photoimg)
+        f_lbl.place(x= -1, y=0)
+
+        # Adding a background image to the application software
+        img1 = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Background.webp")
+        img1 = img1.resize((1530, 710), Image.ANTIALIAS)
+        self.photoimg1 = ImageTk.PhotoImage(img1)
+        bg_img = Label(self.root, image = self.photoimg1)
+        bg_img.place(x = 0, y = 130, width = 1530, height = 710)
+
+        # Adding a text in the application
+        title_lbl = Label(bg_img,text = "Hello there! Welcome", font = ("open sans", 30), bg = "white", fg = "Black")
+        title_lbl.place(x = -100, y = 0, width = 1530, height = 40)
+
+        # Menu Section
+        # Student button
+        img2 = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Student.png")
+        img2 = img2.resize((220, 220), Image.ANTIALIAS)
+        self.photoimg2 = ImageTk.PhotoImage(img2)
+        b1 = Button(bg_img, image = self.photoimg2)
+        b1.place(x = 80, y = 50, width = 220, height = 180)
+        # Text: Student Details
+        b1_1 = Button(bg_img, text = "Student Details", cursor = "hand2", font = ("open sans", 15), bg = "aqua", fg = "Black")
+        b1_1.place(x = 80, y = 230, width = 220, height = 40)
+
+        # Face detection button
+        img3 = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Face-recognition.jpg")
+        img3 = img3.resize((220, 220), Image.ANTIALIAS)
+        self.photoimg3 = ImageTk.PhotoImage(img3)
+        b2 = Button(bg_img, image = self.photoimg3)
+        b2.place(x = 350, y = 50, width = 220, height = 180)
+        # Text: Face Recognition
+        b2_2 = Button(bg_img, text = "Face Recognition", cursor = "hand2", font = ("open sans", 15), bg = "aqua", fg = "Black")
+        b2_2.place(x = 350, y = 230, width = 220, height = 40)
+
+        # Attendance button
+        img4 = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Attendance.jpg")
+        img4 = img4.resize((220, 220), Image.ANTIALIAS)
+        self.photoimg4 = ImageTk.PhotoImage(img4)
+        b3 = Button(bg_img, image = self.photoimg4)
+        b3.place(x = 620, y = 50, width = 220, height = 180)
+        # Text: Attendance
+        b3_3 = Button(bg_img, text = "Attendance", cursor = "hand2", font = ("open sans", 15), bg = "aqua", fg = "Black")
+        b3_3.place(x = 620, y = 230, width = 220, height = 40)
+
+        # Help button
+        img5 = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Help.png")
+        img5 = img5.resize((220, 220), Image.ANTIALIAS)
+        self.photoimg5 = ImageTk.PhotoImage(img5)
+        b4 = Button(bg_img, image = self.photoimg5)
+        b4.place(x = 890, y = 50, width = 220, height = 180)
+        # Text: Help
+        b4_4 = Button(bg_img, text = "Help", cursor = "hand2", font = ("open sans", 15), bg = "aqua", fg = "Black")
+        b4_4.place(x = 890, y = 230, width = 220, height = 40)
+
+        # Train data button
+        img6 = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Train-data.jpg")
+        img6 = img6.resize((220, 220), Image.ANTIALIAS)
+        self.photoimg6 = ImageTk.PhotoImage(img6)
+        b5 = Button(bg_img, image = self.photoimg6)
+        b5.place(x = 80, y = 300, width = 220, height = 180)
+        # Text: Train Data
+        b5_5 = Button(bg_img, text = "Train Data", cursor = "hand2", font = ("open sans", 15), bg = "aqua", fg = "Black")
+        b5_5.place(x = 80, y = 480, width = 220, height = 40)
+
+        # Photos button
+        img7 = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Photos.jpg")
+        img7 = img7.resize((220, 220), Image.ANTIALIAS)
+        self.photoimg7 = ImageTk.PhotoImage(img7)
+        b6 = Button(bg_img, image = self.photoimg7)
+        b6.place(x = 350, y = 300, width = 220, height = 180)
+        # Text: Photos
+        b6_6 = Button(bg_img, text = "Photos", cursor = "hand2", font = ("open sans", 15), bg = "aqua", fg = "Black")
+        b6_6.place(x = 350, y = 480, width = 220, height = 40)
+
+        # Developer button
+        img8 = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Developer.png")
+        img8 = img8.resize((220, 220), Image.ANTIALIAS)
+        self.photoimg8 = ImageTk.PhotoImage(img8)
+        b7 = Button(bg_img, image = self.photoimg8)
+        b7.place(x = 620, y = 300, width = 220, height = 180)
+        # Text: Developer
+        b7_7 = Button(bg_img, text = "Developer", cursor = "hand2", font = ("open sans", 15), bg = "aqua", fg = "Black")
+        b7_7.place(x = 620, y = 480, width = 220, height = 40)
+
+        # Exit button
+        img9 = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Exit.jpg")
+        img9 = img9.resize((220, 220), Image.ANTIALIAS)
+        self.photoimg9 = ImageTk.PhotoImage(img9)
+        b8 = Button(bg_img, image = self.photoimg9)
+        b8.place(x = 890, y = 300, width = 220, height = 180)
+        # Text: Exit
+        b8_8 = Button(bg_img, text = "Exit", cursor = "hand2", font = ("open sans", 15), bg = "aqua", fg = "Black")
+        b8_8.place(x = 890, y = 480, width = 220, height = 40)
+
+if __name__ == "__main__":
+    root = Tk()
+    obj = Face_Recognition_System(root)
+    root.mainloop()
---
+++ student.py
@@ -0,0 +1,228 @@
+from tkinter import * 
+from tkinter import ttk
+from PIL import Image,ImageTk
+
+# Making a class that initiates the backend application of the project
+class Student:
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry("1530x790+0+0")
+        self.root.title("Face Recognition System")
+
+        img = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Header.jpg")
+        img = img.resize((1300, 130), Image.ANTIALIAS)
+        self.photoimg = ImageTk.PhotoImage(img)
+        f_lbl = Label(self.root, image = self.photoimg)
+        f_lbl.place(x= -1, y=0)
+
+        # Adding a background image to the application software
+        img1 = Image.open(r"C:\Users\sohan\Desktop\College\FYP\Facial_Recognition_System\App-Images\Background.webp")
+        img1 = img1.resize((1530, 710), Image.ANTIALIAS)
+        self.photoimg1 = ImageTk.PhotoImage(img1)
+        bg_img = Label(self.root, image = self.photoimg1)
+        bg_img.place(x = 0, y = 130, width = 1530, height = 710)
+
+        # Adding a text in the application
+        title_lbl = Label(bg_img,text = "Student Details", font = ("open sans", 30), bg = "white", fg = "Black")
+        title_lbl.place(x = -100, y = 0, width = 1530, height = 40)
+
+        #Adding a frame
+        main_frame = Frame(bg_img, bd = 2, bg = "white")
+        main_frame.place(x = 25, y = 50, width = 1230, height = 480)
+
+        # left label frame
+        Left_frame = LabelFrame(main_frame, bd = 2, bg = "white", relief = RAISED, text = "Student Details", font = ("Sans Serif", 12))
+        Left_frame.place(x = 10, y = 10, width = 580, height = 450)
+
+        # Information section
+        # Course Info label
+        Course_frame = LabelFrame(Left_frame, bd = 2, bg = "white", relief = RIDGE, text = "Current Course", font = ("Sans Serif", 12))
+        Course_frame.place(x = 5, y = 10, width = 565, height = 100)
+
+        #Course
+        dep_label = Label(Course_frame, text = "Course:", font = ("Sans Serif", 12), bg = "white")
+        dep_label.grid(row = 0, column = 0, padx = 5, sticky = W)
+
+        dep_combo = ttk.Combobox(Course_frame, font = ("Sans Serif", 12), state = "readonly", width = 15)
+        dep_combo["values"] = ("Select Course", "BIT", "BBM", "IMBA")
+        dep_combo.current(0)
+        dep_combo.grid(row = 0, column = 1, padx = 2, pady = 5, sticky = W)
+
+        #Year
+        year_label = Label(Course_frame, text = "Year:", font = ("Sans Serif", 12), bg = "white")
+        year_label.grid(row = 0, column = 2, padx = 5, sticky = W)
+
+        year_combo = ttk.Combobox(Course_frame, font = ("Sans Serif", 12), state = "readonly", width = 15)
+        year_combo["values"] = ("Select Year", "2022-2023", "2023-2024", "2024-2025")
+        year_combo.current(0)
+        year_combo.grid(row = 0, column = 3, padx = 2, pady = 5, sticky = W)
+
+        #Semester
+        semester_label = Label(Course_frame, text = "Semester:", font = ("Sans Serif", 12), bg = "white")
+        semester_label.grid(row = 1, column = 0, padx = 5, sticky = W)
+
+        semester_combo = ttk.Combobox(Course_frame, font = ("Sans Serif", 12), state = "readonly", width = 15)
+        semester_combo["values"] = ("Select Semester", "Fall", "Spring")
+        semester_combo.current(0)
+        semester_combo.grid(row = 1, column = 1, padx = 2, pady = 5, sticky = W)
+
+        #Level
+        level_label = Label(Course_frame, text = "Level:", font = ("Sans Serif", 12), bg = "white")
+        level_label.grid(row = 1, column = 2, padx = 5, sticky = W)
+
+        level_combo = ttk.Combobox(Course_frame, font = ("Sans Serif", 12), state = "readonly", width = 15)
+        level_combo["values"] = ("Select Level", "1", "2", "3", "4", "5", "6")
+        level_combo.current(0)
+        level_combo.grid(row = 1, column = 3, padx = 2, pady = 5, sticky = W)
+
+        #Note: IF the cell is larger than the Widget, the Sticky = W will minimize the problem
+
+
+        # Class Info label
+        class_student_frame = LabelFrame(Left_frame, bd = 2, bg = "white", relief = RIDGE, text = "Class Student Information", font = ("Sans Serif", 12))
+        class_student_frame.place(x = 5, y = 120, width = 565, height = 300)
+
+        #Student ID ############
+        studentID_label = Label(class_student_frame, text = "Student ID:", font = ("Sans Serif", 12), bg = "white")
+        studentID_label.grid(row = 0, column = 0, sticky = W)
+        studentID_entry = ttk.Entry(class_student_frame, width = 15, font = ("Sans Serif", 12))
+        studentID_entry.grid(row = 0, column = 1, pady = 5, sticky = W)
+
+        # Student Name
+        studentName_label = Label(class_student_frame, text = "Student Name:", font = ("Sans Serif", 12), bg = "white")
+        studentName_label.grid(row = 1, column = 0, sticky = W)
+        studentName_entry = ttk.Entry(class_student_frame, width = 15, font = ("Sans Serif", 12))
+        studentName_entry.grid(row = 1, column = 1, pady = 5, sticky = W)
+
+        #Gender ############
+        studentGender_label = Label(class_student_frame, text = "Gender:", font = ("Sans Serif", 12), bg = "white")
+        studentGender_label.grid(row = 2, column = 0, sticky = W)
+        studentGender_entry = ttk.Entry(class_student_frame, width = 15, font = ("Sans Serif", 12))
+        studentGender_entry.grid(row = 2, column = 1, pady = 5, sticky = W)
+
+        #Email ##############
+        studentEmail_label = Label(class_student_frame, text = "Email:", font = ("Sans Serif", 12), bg = "white")
+        studentEmail_label.grid(row = 3, column = 0, sticky = W)
+        studentEmail_entry = ttk.Entry(class_student_frame, width = 15, font = ("Sans Serif", 12))
+        studentEmail_entry.grid(row = 3, column = 1, pady = 5, sticky = W)
+
+        # Parents Name ############
+        studentParentsName_label = Label(class_student_frame, text = "Parents Name:", font = ("Sans Serif", 12), bg = "white")
+        studentParentsName_label.grid(row = 4, column = 0, sticky = W)
+        studentParentsName_entry = ttk.Entry(class_student_frame, width = 15, font = ("Sans Serif", 12))
+        studentParentsName_entry.grid(row = 4, column = 1, pady = 5, sticky = W)
+
+        #Address 
+        studentAddress_label = Label(class_student_frame, text = "Address:", font = ("Sans Serif", 12), bg = "white")
+        studentAddress_label.grid(row = 0, column = 2, padx = 5,  sticky = W)
+        studentAddress_entry = ttk.Entry(class_student_frame, width = 15, font = ("Sans Serif", 12))
+        studentAddress_entry.grid(row = 0, column = 3, padx = 5, sticky = W)
+
+        #Parents Number
+        Pn_label = Label(class_student_frame, text = 'Parents Number: ', font = ("Sans Serif", 12), bg = "white")
+        Pn_label.grid(row = 1, column = 2, padx = 5,  sticky = W)
+        Pn_entry = ttk.Entry(class_student_frame, width = 15, font = ("Sans Serif", 12))
+        Pn_entry.grid(row = 1, column = 3, padx = 5, sticky = W)
+
+
+        #Radio Buttons
+        rbtn1 = ttk.Radiobutton(class_student_frame, text = "Take Photo Sample", value = "Yes")
+        rbtn1.grid(row = 6, column = 0)
+
+        rbtn2 = ttk.Radiobutton(class_student_frame, text = "No Photo Sample")
+        rbtn2.grid(row = 6, column = 1)
+
+        # Buttons Frame
+        btn_frame = Frame(class_student_frame, bd = 2, relief = RAISED, bg = "white")
+        btn_frame.place(x = 3, y =200, width = 555, height = 37)
+
+        savebtn = Button(btn_frame, text = "Save", width = 13, font = ("Sans Serif", 12), bg = "aqua", fg = "black")
+        savebtn.grid(row = 0, column = 0, padx= 5)
+
+        updatebtn = Button(btn_frame, text = "Update", width = 13, font = ("Sans Serif", 12), bg = "aqua", fg = "black")
+        updatebtn.grid(row = 0, column = 1, padx= 5)
+
+        deletebtn = Button(btn_frame, text = "Delete", width = 13, font = ("Sans Serif", 12), bg = "aqua", fg = "black")
+        deletebtn.grid(row = 0, column = 2, padx= 5)
+
+        resetbtn = Button(btn_frame, text = "Reset", width = 13, font = ("Sans Serif", 12), bg = "aqua", fg = "black")
+        resetbtn.grid(row = 0, column = 3, padx= 5)
+
+        btn_frame1 = Frame(class_student_frame, bd = 2, relief = RAISED, bg = "white")
+        btn_frame1.place(x = 3, y =235, width = 555, height = 37)
+
+
+        takephbtn = Button(btn_frame1, text = "Take photo sample", width = 29, font = ("Sans Serif", 12), bg = "aqua", fg = "black")
+        takephbtn.grid(row = 0, column = 2)
+
+        updatephbtn = Button(btn_frame1, text = "Update photo sample", width = 29, font = ("Sans Serif", 12), bg = "aqua", fg = "black")
+        updatephbtn.grid(row = 0, column = 3)
+
+
+        ############ Right label frame ############
+        Right_frame = LabelFrame(main_frame, bd = 2, bg = "white", relief = RAISED, text = "Student Details", font = ("Sans Serif", 12))
+        Right_frame.place(x = 630, y = 10, width = 580, height = 450)
+        #=====Search Frame=====
+        search_frame = LabelFrame(Right_frame, bd = 2, bg = "white", relief = RIDGE, text = "Search", font = ("Sans Serif", 12))
+        search_frame.place(x = 5, y = 10, width = 565, height = 70)
+        search_label = Label(search_frame, text = "Search By: ", font = ("Sans Serif", 12), bg = "white")
+        search_label.grid(row = 0, column = 0, padx = 5,  sticky = W)
+        ### Combo Boxes
+        search_combo = ttk.Combobox(search_frame, font = ("Sans Serif", 12), state = "readonly", width = 10)
+        search_combo["values"] = ("Select", "Name", "ID", "Email")
+        search_combo.current(0)
+        search_combo.grid(row = 0, column = 1, padx = 2, pady = 5, sticky = W)
+        ### Entry Fields
+        search_entry = ttk.Entry(search_frame, width = 15, font = ("Sans Serif", 12))
+        search_entry.grid(row = 0, column = 2, pady = 5, sticky = W)
+        ### Buttons
+        searchbtn = Button(search_frame, text = "Search", width = 10, font = ("Sans Serif", 12), bg = "aqua", fg = "black")
+        searchbtn.grid(row = 0, column = 3, padx = 2)
+        showallbtn = Button(search_frame, text = "Show All", width = 10, font = ("Sans Serif", 12), bg = "aqua", fg = "black")
+        showallbtn.grid(row = 0, column = 4, padx = 2)
+
+        # ===== Table Frame ====
+        table_frame = LabelFrame(Right_frame, bd = 2, bg = "white", relief = RIDGE)
+        table_frame.place(x = 5, y = 120, width = 565, height = 300)
+    
+        Scroll_x = ttk.Scrollbar(table_frame, orient = HORIZONTAL)
+        Scroll_y = ttk.Scrollbar(table_frame, orient = VERTICAL)
+        self.student_table = ttk.Treeview(table_frame, column = ("Student ID", "Student Name", "Email", "Address", "Parents Name", "Parents Number"), xscrollcommand = Scroll_x.set, yscrollcommand = Scroll_y.set)
+        Scroll_x.pack(side = BOTTOM, fill = X)
+        Scroll_y.pack(side = RIGHT, fill = Y)
+        Scroll_x.config(command = self.student_table.xview)
+        Scroll_y.config(command = self.student_table.yview)
+        self.student_table.heading("Student ID", text = "Student ID")
+        self.student_table.heading("Student Name", text = "Student Name")
+        self.student_table.heading("Email", text = "Email")
+        self.student_table.heading("Address", text = "Address")
+        self.student_table.heading("Parents Name", text = "Parents Name")
+        self.student_table.heading("Parents Number", text = "Parents Number")
+        self.student_table["show"] = "headings"
+
+        self.student_table.column("Student ID", width = 100)
+        self.student_table.column("Student Name", width = 100)
+        self.student_table.column("Email", width = 100)
+        self.student_table.column("Address", width = 100)
+        self.student_table.column("Parents Name", width = 100)
+        self.student_table.column("Parents Number", width = 100)
+
+
+
+
+
+        self.student_table.pack(fill = BOTH, expand = 1)
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    obj = Student(root)
+    root.mainloop()
