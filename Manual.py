e159b68d0a5a648d12b34bd82175b62d41ebd97b
Suhan Shrestha <83650068+Sohann-jpg@users.noreply.github.com>
2023-04-18 14:32:07 +0545

    Manual Attendance Page update

--- Manual.py
+++ Manual.py
@@ -3,8 +3,7 @@ from tkinter import ttk
 from PIL import Image,ImageTk
 from tkinter import messagebox
 from tkcalendar import DateEntry
-import csv
-import os
+import mysql.connector
 # Making a class that initiates the backend application of the project
 class Student:
     def __init__(self,root):
@@ -13,13 +12,12 @@ class Student:
         self.root.title("Face Recognition System")
 
         ####Variables####
-        self.var_level = StringVar()
         self.var_sID = StringVar()
         self.var_date = StringVar()
-        self.var_Time = StringVar()
-        self.var_tutorID = StringVar()
-        self.var_roomID = StringVar()
-        self.var_module_code = StringVar()
+        self.var_time = StringVar()
+        self.var_name = StringVar()
+        self.var_course = StringVar()
+        self.var_attenstats = StringVar()
 
         # ===== Header and background images =====
         img = Image.open(r"App-Images\Header.jpg")
@@ -57,29 +55,32 @@ class Student:
         # TIme
         Time_label = Label(class_student_frame, text = "Time:", font = ("Sans Serif", 12), bg = "white")
         Time_label.grid(row = 1, column = 0, sticky = W)
-        Time_entry = ttk.Entry(class_student_frame, textvariable= self.var_Time, width = 15, font = ("Sans Serif", 12))
+        Time_entry = ttk.Entry(class_student_frame, textvariable= self.var_time, width = 15, font = ("Sans Serif", 12))
         Time_entry.grid(row = 1, column = 1, pady = 5, sticky = W)
 
-        # Level
-        level_label = Label(class_student_frame, text = "Level:", font = ("Sans Serif", 12), bg = "white")
-        level_label.grid(row = 2, column = 0, sticky = W)
-        # Level Combo box
-        level_combo = ttk.Combobox(class_student_frame, textvariable = self.var_level, font = ("Sans Serif", 12), state = "readonly", width = 13)
-        level_combo["values"] = ("Select Level", "1", "2", "3", "4", "5", "6")
-        level_combo.current(0)
-        level_combo.grid(row = 2, column = 1, pady = 3, sticky = W)
+        # name
+        name_label = Label(class_student_frame, text = "Name:", font = ("Sans Serif", 12), bg = "white")
+        name_label.grid(row = 2, column = 0, sticky = W)
+        name_entry = ttk.Entry(class_student_frame, textvariable= self.var_name, width = 15, font = ("Sans Serif", 12))
+        name_entry.grid(row = 2, column = 1, pady = 5, sticky = W)
 
-        # Tutor ID
-        tutor_ID_label = Label(class_student_frame, text = "Tutor ID:", font = ("Sans Serif", 12), bg = "white")
-        tutor_ID_label.grid(row = 3, column = 0, sticky = W)
-        tutor_ID_entry = ttk.Entry(class_student_frame, textvariable= self.var_tutorID, width = 15, font = ("Sans Serif", 12))
-        tutor_ID_entry.grid(row = 3, column = 1, pady = 5, sticky = W)
+        # course
+        course_label = Label(class_student_frame, text = "Course:", font = ("Sans Serif", 12), bg = "white")
+        course_label.grid(row = 4, column = 0, sticky = W)
+        # course Combo box
+        course_combo = ttk.Combobox(class_student_frame, textvariable = self.var_course, font = ("Sans Serif", 12), state = "readonly", width = 13)
+        course_combo["values"] = ("Select Course", "BIT", "BIM", "BBM")
+        course_combo.current(0)
+        course_combo.grid(row = 4, column = 1, pady = 5, sticky = W)
 
-        # Room ID
-        room_ID_label = Label(class_student_frame, text = "Room ID:", font = ("Sans Serif", 12), bg = "white")
-        room_ID_label.grid(row = 4, column = 0, sticky = W)
-        room_ID_entry = ttk.Entry(class_student_frame, textvariable= self.var_roomID, width = 15, font = ("Sans Serif", 12))
-        room_ID_entry.grid(row = 4, column = 1, pady = 5, sticky = W)
+        #Attendance Status
+        attendance_label = Label(class_student_frame, text = "Attendance Satus:", font = ("Sans Serif", 12), bg = "white")
+        attendance_label.grid(row = 1, column = 2, sticky = W)
+        # Attendance Combo box
+        attendance_combo = ttk.Combobox(class_student_frame, textvariable = self.var_attenstats, font = ("Sans Serif", 12), state = "readonly", width = 14)
+        attendance_combo["values"] = ("Attendance Status", "Present", "Absent")
+        attendance_combo.current(0)
+        attendance_combo.grid(row = 1, column = 3, pady = 5, sticky = W)
 
         # Date 
         date_label = Label(class_student_frame, text = "Date:", font = ("Sans Serif", 12), bg = "white")
@@ -87,11 +88,6 @@ class Student:
         cal = DateEntry(class_student_frame, width= 20, textvariable = self.var_date, background='darkblue', foreground='white', borderwidth=2)
         cal.grid(row=0, column = 3, padx= 5, sticky = W)
 
-        # Module Code
-        module_code_label = Label(class_student_frame, text = 'Module Code: ', font = ("Sans Serif", 12), bg = "white")
-        module_code_label.grid(row = 1, column = 2, padx = 5,  sticky = W)
-        module_code_entry = ttk.Entry(class_student_frame, textvariable= self.var_module_code, width = 15, font = ("Sans Serif", 12))
-        module_code_entry.grid(row = 1, column = 3, padx = 5, sticky = W)
 
         # ===== Functional Buttons 1.0 =====
         # Buttons Frame
@@ -102,8 +98,8 @@ class Student:
         updatebtn = Button(btn_frame, text = "Update", command = self.update_student_table, width = 13, font = ("Sans Serif", 12), bg = "#117c9d", fg = "black")
         updatebtn.place(x = 0, y = 0, width= 198)
 
-        # Delete 
-        export_csv_btn = Button(btn_frame, text = "Export CSV", command = self.export_student_data, width = 13, font = ("Sans Serif", 12), bg = "#117c9d", fg = "black")
+        # # Delete 
+        export_csv_btn = Button(btn_frame, text = "Delete", command = self.delete_data, width = 13, font = ("Sans Serif", 12), bg = "#117c9d", fg = "black")
         export_csv_btn.place(x = 200, y = 0, width= 198)
 
         # Reset
@@ -127,76 +123,91 @@ class Student:
         # ===== Scroll bar =====
 
         # ===== Table Contents =====
-        self.student_table = ttk.Treeview(table_frame, column = ("Student ID", "Date", "Time", "Module Code", "Level", "Tutor ID", "Room ID"), xscrollcommand = Scroll_x.set, yscrollcommand = Scroll_y.set)
+        self.student_table = ttk.Treeview(table_frame, column = ("Student ID", "Name", "Course", "Time", "Date", "Attendance Status"), xscrollcommand = Scroll_x.set, yscrollcommand = Scroll_y.set)
         Scroll_x.pack(side = BOTTOM, fill = X)
         Scroll_y.pack(side = RIGHT, fill = Y)
         Scroll_x.config(command = self.student_table.xview)
         Scroll_y.config(command = self.student_table.yview)     
         self.student_table.heading("Student ID", text = "Student ID")
-        self.student_table.heading("Date", text = "Date")
+        self.student_table.heading("Name", text = "Name")
+        self.student_table.heading("Course", text = "Course")
         self.student_table.heading("Time", text = "Time")
-        self.student_table.heading("Module Code", text = "Module Code")
-        self.student_table.heading("Level", text = "Level")
-        self.student_table.heading("Tutor ID", text = "Tutor ID")
-        self.student_table.heading("Room ID", text = "Room ID")
+        self.student_table.heading("Date", text = "Date")
+        self.student_table.heading("Attendance Status", text = "Attendance Status")
         self.student_table["show"] = "headings"
 
         self.student_table.column("Student ID", width = 100)
-        self.student_table.column("Date", width = 100)
+        self.student_table.column("Name", width = 100)
+        self.student_table.column("Course", width = 100)
         self.student_table.column("Time", width = 100)
-        self.student_table.column("Module Code", width = 100)
-        self.student_table.column("Level", width = 100)
-        self.student_table.column("Tutor ID", width = 100)
-        self.student_table.column("Room ID", width = 100)
+        self.student_table.column("Date", width = 100)
+        self.student_table.column("Attendance Status", width = 100)
         self.student_table.pack(fill = BOTH, expand = 1)
         self.student_table.bind("<ButtonRelease>")
-        # self.fetch_data()
         # ===== Table Contents =====
-
-    def update_student_table(self):
-        level = self.var_level.get()
-        std_ID = self.var_sID.get()
-        Date = self.var_date.get()
-        Time = self.var_Time.get()
-        t_ID = self.var_tutorID.get()
-        r_ID = self.var_roomID.get()
-        m_code = self.var_module_code.get()
-        # Insert the data into the treeview
-        self.student_table.insert("", "end", values=(std_ID, Date, Time, m_code, level, t_ID, r_ID))
 
     def reset_data(self):
         self.var_sID.set("")
+        self.var_name.set("")
+        self.var_course.set("")
         self.var_date.set("Date")
-        self.var_Time.set("")
-        self.var_level.set("Select Level")
-        self.var_tutorID.set("")
-        self.var_roomID.set("")
-        self.var_module_code.set("")
+        self.var_time.set("")
+        self.var_attenstats.set("")
 
-    def export_student_data(self):
-        file_exists = os.path.isfile("student_data.csv")
-        # Open a file for writing
-        with open("student_data.csv", "a", newline="") as file:
-            writer = csv.writer(file)
-            
-            # Write the header row
-            if not file_exists:
-                writer.writerow(["Student ID", "Date", "Time", "Module Code", "Level", "Tutor ID", "Room ID"])
-            
-            # Write each row of data from the treeview
-            for row in self.student_table.get_children():
-                std_ID = self.student_table.item(row, "values")[0]
-                Date = self.student_table.item(row, "values")[1]
-                Time = self.student_table.item(row, "values")[2]
-                m_code = self.student_table.item(row, "values")[3]
-                level = self.student_table.item(row, "values")[4]
-                t_ID = self.student_table.item(row, "values")[5]
-                r_ID = self.student_table.item(row, "values")[6]
-                writer.writerow([std_ID, Date, Time, m_code, level, t_ID, r_ID])
-        try:
-            messagebox.showinfo("Export CSV", "CSV file exported successfully!")
-        except:
-            messagebox.showerror("Export CSV", "Error exporting CSV file.")
+    def update_student_table(self):
+        std_ID = self.var_sID.get()
+        name = self.var_name.get()
+        Course = self.var_course.get()
+        Date = self.var_date.get()
+        Time = self.var_time.get()
+        attendance_status = self.var_attenstats.get()
+        # Insert the data into the treeview
+        self.student_table.insert("", "end", values=(std_ID, name, Course, Date, Time, attendance_status))
+        if self.var_name.get() == "" or self.var_time.get() == "" or self.var_sID.get() == "":
+            messagebox.showerror("Error", "All fields required", parent = self.root)
+        else:
+            try:
+                Update = messagebox.askyesno("update", "Do you want to update the attendance?", parent = self.root)
+                if Update > 0:
+                    conn = mysql.connector.connect(host = "localhost", username = "root", password = "PunnSxG@2806", database = "face_recognition")
+                    my_cursor = conn.cursor()
+                    my_cursor.execute("insert into attendance values(%s,%s,%s,%s,%s,%s)",(
+                                                                                        self.var_sID.get(),
+                                                                                        self.var_name.get(),
+                                                                                        self.var_course.get(),
+                                                                                        self.var_time.get(),
+                                                                                        self.var_date.get(),
+                                                                                        self.var_attenstats.get()                                                                                    
+                                                                                    ))
+                else:
+                    if not Update:
+                        return
+                messagebox.showinfo("Success", "Attendance successfully updated", parent = self.root)
+                conn.commit()
+                conn.close()
+            except Exception as es:
+                messagebox.showerror("Error", f"Due to: {str(es)}", parent = self.root)
+
+    def delete_data(self):
+        if self.var_sID.get() == "":
+            messagebox.showerror("Error", "Student ID required", parent = self.root)
+        else:
+            try:
+                delete = messagebox.askyesno("Student Delete Page", "Do you really want to delete this entry?", parent = self.root)
+                if delete > 0:
+                    conn = mysql.connector.connect(host = "localhost", username = "root", password = "PunnSxG@2806", database = "face_recognition")
+                    my_cursor = conn.cursor()
+                    sql = "delete from attendance where student_id = %s"
+                    val = (self.var_sID.get(),)
+                    my_cursor.execute(sql, val)
+                else:
+                    if not delete:
+                        return
+                conn.commit()
+                conn.close()
+                messagebox.showinfo("Delete", "Successfully deleted entry", parent = self.root)
+            except Exception as es:
+                messagebox.showerror("Error", f"Due to: {str(es)}", parent = self.root)
 
 if __name__ == "__main__":
     root = Tk()
